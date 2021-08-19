63f83bf19697348d1bb881dcee752a93ccbd6397
srikant_pycharm <srikant.mar1984@gmail.com>
2021-08-20 01:01:47 +0530

    first trail

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Function
+"""
+
+
+def calculate(a, b):
+    total = 0
+    total = a + b
+
+    return total
+
+
+d_telephone = {'rupa': 9497, 'srikant': 7528}
+n = calculate(19, 13)
+print("19_13:", n)
+n = calculate(34, 78)
+print("34_78:", n)
+
