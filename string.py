06247d68df986e66039b72ea97a4f92e6d02bd74
srikant_pycharm <srikant.mar1984@gmail.com>
2021-08-20 00:56:52 +0530

    first trail

---
+++ string.py
@@ -0,0 +1,24 @@
+ name = input("Enter Your name")
+
+#
+# print(name[1:])
+# print(name[0:3])
+# print(name[:4])
+# print(name[-1])
+# print(name[-4:-1])
+# print(name*4)
+# print('t' in name)
+#print(r'C:\\user\srikant')
+
+# age = input("Enter Your age")
+# occp = input("Enter Your occupation")
+# print("my name is %s."%(name),"i am %s years old."%(age),"i am a %s."%(occp))
+
+
+#print("""They Said "who are you?" """)
+#print("""They Said, "What\'s going on?" """)
+
+# print("{} and {} both are the best friend".format("Devansh","Abhishek"))
+# print("{1} and {0} best players ".format("Virat","Rohit"))
+# print("{c},{b},{a}".format(a = "James", b = "Peter", c = "Ricky"))
+
